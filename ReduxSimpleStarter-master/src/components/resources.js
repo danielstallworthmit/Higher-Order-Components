5a96d3f4ae49d5135c44066e1b2e6ca9578732b9
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-18 19:24:34 -0700

    Add header and resources route

---
+++ ReduxSimpleStarter-master/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Secret Recipe
+            <ul>
+                <li> 1 cup Secret</li>
+                <li> 1 cup Special</li>
+                <li> 1 cup Super</li>
+            </ul>
+        </div>
+    );
+}
